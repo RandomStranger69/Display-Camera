83312b960d84936c9e1e7135b53b331c2df42f2d
RandomStranger69 <162474602+RandomStranger69@users.noreply.github.com>
2024-05-19 18:27:24 +0800

    moves all files into root directory instead

--- templates/filesearcher.py
+++
@@ -1,8 +0,0 @@
-import os
-
-cascade_path = r"C:\Users\Angelo\Downloads\haarcascade_frontalface_default.xml"
-
-if os.path.exists(cascade_path):
-    print("File exists and is accessible.")
-else:
-    print("File does not exist or is not accessible.")
